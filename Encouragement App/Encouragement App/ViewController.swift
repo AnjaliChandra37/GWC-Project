85677859e59d4ff6052397919a9f575cc596c5f3
AnjaliChandra37 <51925786+AnjaliChandra37@users.noreply.github.com>
2019-07-29 13:43:54 -0700

    Add files via upload

---
+++ Encouragement App/Encouragement App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Encouragement App
+//
+//  Created by GWC on 7/29/19.
+//  Copyright © 2019 PG4Robotics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
